31abd224b9d4c0a6cbfbf0165a67df6789ac36e6
David Kellum <dek-oss@gravitext.com>
2019-07-19 12:35:44 -0700

    misc improvements

---
+++ README.md
@@ -0,0 +1,22 @@
+# test-thread-id
+
+Experiment with use of rust libstd `RandomState` for seeding PRNGs.
+
+Concurrently contributed as tokio-rs/tokio#1324 by the same author, under the
+MIT license.
+
+## License
+
+This project is dual licensed under either of following:
+
+* The Apache License, version 2.0 ([LICENSE-APACHE](LICENSE-APACHE)
+  or http://www.apache.org/licenses/LICENSE-2.0)
+
+* The MIT License ([LICENSE-MIT](LICENSE-MIT)
+  or http://opensource.org/licenses/MIT)
+
+### Contribution
+
+Unless you explicitly state otherwise, any contribution intentionally submitted
+for inclusion in tardy-sink by you, as defined by the Apache License, shall be
+dual licensed as above, without any additional terms or conditions.
--- src/main.rs
+++ src/main.rs
@@ -5,36 +5,61 @@ use std::hash::{BuildHasher, Hash, Hasher};
 use lazy_static::lazy_static;
 
 fn main() {
-    let tid = thread::current().id();
-    println!("main thread id: {:?}, seed: {:08x} {:08x}",
-             tid, prng_seed(), prng_seed());
+    println!("zero seed out: {}", sample(0));
+    println!("one  seed out: {}", sample(1));
 
-    for _ in 0..8 {
-        thread::spawn(|| {
+    let tid = thread::current().id();
+    let ms = sample(prng_seed());
+    println!("main  thread id: {:?}, seed: {:08x}, out: {}",
+             tid, prng_seed(), ms);
+    (1..9)
+        .map(|l| thread::spawn(move || {
             let tid = thread::current().id();
-            println!("alt  thread id: {:?}, seed: {:08x} {:08x}",
-                     tid, prng_seed(), prng_seed());
-        }).join().unwrap();
-    }
+            let ts = sample(prng_seed());
+            println!("alt {} thread id: {:?}, seed: {:08x}, out: {}",
+                     l, tid, prng_seed(), ts);
+        }))
+        .for_each(|t| t.join().unwrap());
 }
 
-
-// Piggyback on libstd's mechanism for obtaining system randomness for default
-// hasher, and hash on current thread id for a seed value.
+// Return a thread-specific, 32-bit, non-zero seed value suitable for a 32-bit
+// PRNG. This uses one libstd RandomState for a default hasher and hashes on
+// the current thread ID to obtain an unpredictable, collision resistant seed.
 fn prng_seed() -> u32 {
+    // This obtains a small number of random bytes from the host system (for
+    // example, on unix via getrandom(2)) in order to seed an unpredictable and
+    // HashDoS resistant 64-bit hash function (currently: `SipHasher13` with
+    // 128-bit state). We only need one of these, to make the seeds for all
+    // process threads different via hashed IDs, collision resistant, and
+    // unpredictable.
     lazy_static! {
         static ref RND_STATE: RandomState = RandomState::new();
     }
 
+    // Hash the current thread ID to produce a u32 value
     let mut hasher = RND_STATE.build_hasher();
     thread::current().id().hash(&mut hasher);
     let hash: u64 = hasher.finish();
     let seed = (hash as u32) ^ ((hash >> 32) as u32);
 
-    // ensure non-zero
+    // Ensure non-zero seed (Xorshift yields only zeroes for that seed)
     if seed == 0 {
-        0x9b4e_6d25 // misc bits
+        0x9b4e_6d25 // misc bits, could be any non-zero
     } else {
         seed
     }
+}
+
+fn sample(mut x: u32) -> String {
+    (0..10)
+        .map(|_| { x = xorshift(x); x })
+        .map(|v| format!("{:08x}", v))
+        .collect::<Vec<String>>()
+        .join(" ")
+}
+
+fn xorshift(mut x: u32) -> u32 {
+    x ^= x << 13;
+    x ^= x >> 17;
+    x ^ x << 5
 }
